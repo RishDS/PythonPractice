28eacd4cebed305707cb60e019c45a30c61c6800
Rishbha Godara <124633580+RishDS@users.noreply.github.com>
2024-02-13 20:11:18 -0800

    Update W3-makeBricks.py

--- W3-makeBricks.py
+++ W3-makeBricks.py
@@ -1,3 +1,7 @@
+# Problem Statement: We want to make a row of bricks that is goal inches long. 
+# We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+# Return true if it is possible to make the goal by choosing from the given bricks.
+
 def make_bricks(small, big, goal):
     
     big_needed = goal / 5
